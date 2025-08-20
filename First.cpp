267761dac41cdeb8dcc388d343de40c6412d6f0a
hsl <2276524341@qq.com>
2025-08-20 17:06:41 +0800

    Branch hsl02

--- First.cpp
+++ First.cpp
@@ -2,7 +2,8 @@
 
 
 int main(){
-    std::cout<<"this is th first prooject"<<std::endl;
-    
+    system("chcp 65001"); // 设置终端编码为UTF-8
+    std::cout<<"我打开了一个新的分支"<<std::endl;
+    std::cout<<"hello hsl，你好 hsl，hello world，你好 世界"<<std::endl;
     return 0;
 }
